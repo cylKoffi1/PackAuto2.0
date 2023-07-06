89e29845450ade4bdc4c7edcfa17f86fdc6664c3
cylko <cylko@DESKTOP-BETSA01>
2023-07-06 21:14:23 +0000

    Fin accueil park auto

--- PackAuto/src/packauto/Bienvenu.java
+++ PackAuto/src/packauto/Bienvenu.java
@@ -29,7 +29,7 @@ public class Bienvenu extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        ok = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -42,14 +42,14 @@ public class Bienvenu extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
         jLabel2.setText("Bienvenue sur Sweetloc");
 
-        jButton1.setBackground(new java.awt.Color(102, 0, 0));
-        jButton1.setFont(new java.awt.Font("Microsoft JhengHei Light", 1, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/sourire.png"))); // NOI18N
-        jButton1.setText("C'est partie");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        ok.setBackground(new java.awt.Color(102, 0, 0));
+        ok.setFont(new java.awt.Font("Microsoft JhengHei Light", 1, 12)); // NOI18N
+        ok.setForeground(new java.awt.Color(255, 255, 255));
+        ok.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/sourire.png"))); // NOI18N
+        ok.setText("C'est partie");
+        ok.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                okActionPerformed(evt);
             }
         });
 
@@ -59,7 +59,7 @@ public class Bienvenu extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(ok)
                 .addGap(93, 93, 93))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -78,7 +78,7 @@ public class Bienvenu extends javax.swing.JFrame {
                 .addGap(35, 35, 35)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 92, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(ok)
                 .addGap(18, 18, 18))
         );
 
@@ -98,9 +98,10 @@ public class Bienvenu extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void okActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okActionPerformed
+        dispose();
+        new ParkA().setVisible(true);        
+    }//GEN-LAST:event_okActionPerformed
 
     /**
      * @param args the command line arguments
@@ -138,9 +139,9 @@ public class Bienvenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton ok;
     // End of variables declaration//GEN-END:variables
 }
